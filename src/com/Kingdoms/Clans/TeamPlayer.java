392cb3f0454b3e486f48369b3cf5534c3c81ea14
unknown <Steve@Balthasar.(none)>
2011-12-22 19:36:20 -0500

    Added TeamPlayer Code

--- src/com/Kingdoms/Clans/TeamPlayer.java
+++ src/com/Kingdoms/Clans/TeamPlayer.java
@@ -1,5 +1,21 @@
 package com.Kingdoms.Clans;
 
 public class TeamPlayer {
-
+	
+	private int ELO;
+	private int LastSeen;
+	
+	private String TeamKey;
+	private String Invite;
+	
+	private boolean TeamKillSetting;
+	
+	//For Loading from a file at start up
+	TeamPlayer(int ELOin, int LastSeenin)
+	{
+		ElO = ELOin;
+		LastSeen = LastSeenin;
+	}
+	
+	
 }
